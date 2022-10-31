fe2622983d05dda27d3a6d95ce28e3374240f5c7
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-10-31 17:37:57 -0300

    criado o reducer para wallet e componente header para a pagina da carteira

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
+  state = {
+    selectedCurrency: [],
+    expenseValue: 0,
+  };
+
+  componentDidMount() {
+    const { currencies, expenses } = this.props;
+    const expenseValue = expenses.length === 0 ? 0 : 1;
+    this.setState({
+      selectedCurrency: currencies[0],
+      expenseValue,
+    });
+  }
+
   render() {
+    const { expenseValue, selectedCurrency } = this.state;
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">{expenseValue}</p>
+        <p data-testid="header-currency-field">{selectedCurrency}</p>
+      </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  currencies: state.wallet.currencies,
+  expenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  currencies: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
